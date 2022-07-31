cc783bc896ad468760bd3f3d19d54ddc14843a19
Hojinida <opqrs1103@gmail.com>
2022-08-01 07:12:38 +0900

    test: git ingnore

--- src/main/java/com/jaks1m/project/domain/api/entity/board/BoardOrderType.java
+++
@@ -1,5 +0,0 @@
-package com.jaks1m.project.domain.api.entity.board;
-
-public enum BoardOrderType {
-    NEW,LIKE,COMMENT,VIEW
-}
--- src/main/java/com/jaks1m/project/domain/api/entity/user/Status.java
+++
@@ -1,6 +0,0 @@
-package com.jaks1m.project.domain.api.entity.user;
-
-
-public enum Status {
-    ACTIVE,DELETE
-}
