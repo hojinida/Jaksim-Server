488530079eb64150d643995f5994c2cfec00d0a1
Hojinida <opqrs1103@gmail.com>
2022-08-01 14:56:46 +0900

    fix: deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
 
-PROJECT_ROOT="/home/ubuntu/app/test"
-JAR_FILE="$PROJECT_ROOT/jaksim.jar"
+# 자주 사용하는 값 변수에 저장
+REPOSITORY=/home/ubuntu/app/test
+PROJECT_NAME=jaksim
 
-APP_LOG="$PROJECT_ROOT/application.log"
-ERROR_LOG="$PROJECT_ROOT/error.log"
-DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+# git clone 받은 위치로 이동
+cd $REPOSITORY/$PROJECT_NAME/
 
-TIME_NOW=$(date +%c)
+# master 브랜치의 최신 내용 받기
+echo "> Git Pull"
+git pull
 
-# build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+# build 수행
+echo "> project build start"
+./gradlew build
 
-# jar 파일 실행
-echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+echo "> directory로 이동"
+cd $REPOSITORY
 
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+# build의 결과물 (jar 파일) 특정 위치로 복사
+echo "> build 파일 복사"
+cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
+
+echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
+if [ -z "$CURRENT_PID" ]; then
+	echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+	echo "> kill -15 $CURRENT_PID"
+	kill -15 $CURRENT_PID
+	sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
+
+echo "> Jar Name: $JAR_NAME"
+nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &
