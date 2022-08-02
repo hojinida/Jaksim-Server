9a262c8bfb91ce6d6e73975c1c2c549e5095ba02
Hojinida <opqrs1103@gmail.com>
2022-08-03 07:45:47 +0900

    fix: deploy.yml

--- src/main/java/com/jaks1m/project/domain/config/properties/AwsS3Config.java
+++ src/main/java/com/jaks1m/project/domain/config/properties/AwsS3Config.java
@@ -2,7 +2,7 @@ package com.jaks1m.project.domain.config.properties;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -20,15 +20,11 @@ public class AwsS3Config {
     private String region;
 
     @Bean
-    public BasicAWSCredentials basicAWSCredentials() {
-        return new BasicAWSCredentials(accessKey, secretKey);
-    }
-
-    @Bean
-    public AmazonS3 amazonS3(BasicAWSCredentials basicAWSCredentials) {
-        return AmazonS3ClientBuilder.standard()
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey,secretKey);
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
                 .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                 .build();
     }
 }
