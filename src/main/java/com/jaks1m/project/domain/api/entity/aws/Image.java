31345d1779a8bfd37746fde6a182cbedddae2963
Hojinida <opqrs1103@gmail.com>
2022-08-03 23:28:47 +0900

    fix: image generate id

--- src/main/java/com/jaks1m/project/domain/api/entity/aws/Image.java
+++ src/main/java/com/jaks1m/project/domain/api/entity/aws/Image.java
@@ -1,5 +1,6 @@
 package com.jaks1m.project.domain.api.entity.aws;
 
+import com.jaks1m.project.domain.api.entity.user.BaseEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,9 +11,9 @@ import javax.persistence.*;
 @Entity
 @NoArgsConstructor
 @SequenceGenerator(name = "IMAGE_SEQ_GENERATOR", sequenceName = "IMAGE_SEQ")
-public class Image {
+public class Image extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "BOARD_SEQ_GENERATOR")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "IMAGE_SEQ_GENERATOR")
     @Column(name = "IMAGE_ID")
     private Long id;
     private String key;
