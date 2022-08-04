04819025947ff9699efb5dc69ad940ab90e6e189
Hojinida <opqrs1103@gmail.com>
2022-08-04 09:21:21 +0900

    test: schedule

--- src/main/java/com/jaks1m/project/domain/common/Schedule.java
+++ src/main/java/com/jaks1m/project/domain/common/Schedule.java
@@ -1,14 +1,16 @@
 package com.jaks1m.project.domain.common;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class Schedule {
     //@Scheduled(cron = "0 1 9 0 * ?")
     @Scheduled(fixedRate = 1000)
     public void deleteUser(){
-        System.out.println("schedule start");
+        log.info("122313123123123123123");
     }
 }
