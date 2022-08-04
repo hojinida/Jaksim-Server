7e383e2a3fa49c92f1e9c7b3a3cf77611aaa6825
Hojinida <opqrs1103@gmail.com>
2022-08-04 20:06:19 +0900

    fix: cron

--- src/main/java/com/jaks1m/project/domain/common/Schedule.java
+++ src/main/java/com/jaks1m/project/domain/common/Schedule.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class Schedule {
     private final UserRepository userRepository;
     private final BoardRepository boardRepository;
-    @Scheduled(cron = "0 1 9 0 * ?")//매월 1일 9시 0분에 실행
+    @Scheduled(cron = "0 0 18 L * ?")//매월 말일 18시 0분에 실행
     public void deleteUser(){
         log.info("Schedule User Delete Run");
         List<User> users = userRepository.findAllByStatusAndLastModifiedDateAfter(Status.DELETE, LocalDateTime.now().minusYears(5));
