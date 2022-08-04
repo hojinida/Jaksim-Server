d2aeb80fd44ee27924b96d3cc68f3037e702843b
Hojinida <opqrs1103@gmail.com>
2022-08-04 23:05:43 +0900

    feat: cors disable

--- src/main/java/com/jaks1m/project/domain/config/security/WebSecurityConfig.java
+++ src/main/java/com/jaks1m/project/domain/config/security/WebSecurityConfig.java
@@ -31,6 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                    .cors().disable()
                     .csrf().disable()
                     .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
