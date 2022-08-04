958f4f48fda700ca5bc74edb8e4e74c0fac922a1
Hojinida <opqrs1103@gmail.com>
2022-08-04 10:36:47 +0900

    test: test schdule

--- src/main/java/com/jaks1m/project/domain/common/Schedule.java
+++ src/main/java/com/jaks1m/project/domain/common/Schedule.java
@@ -1,16 +1,31 @@
 package com.jaks1m.project.domain.common;
 
+import com.jaks1m.project.domain.api.entity.board.Board;
+import com.jaks1m.project.domain.api.entity.user.Status;
+import com.jaks1m.project.domain.api.entity.user.User;
+import com.jaks1m.project.domain.api.repository.BoardRepository;
+import com.jaks1m.project.domain.api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@Component
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Slf4j
+@Component
+@RequiredArgsConstructor
 public class Schedule {
-    //@Scheduled(cron = "0 1 9 0 * ?")
-    @Scheduled(fixedRate = 1000)
+    private final UserRepository userRepository;
+    private final BoardRepository boardRepository;
+    @Scheduled(cron = "0 1 9 0 * ?")//매월 1일 9시 0분에 실행
     public void deleteUser(){
-        log.info("122313123123123123123");
+        log.info("Schedule User Delete Run");
+        List<User> users = userRepository.findAllByStatusAndLastModifiedDateBefore(Status.DELETE, LocalDateTime.now().minusYears(5));
+        userRepository.deleteAll(users);
+        log.info("Schedule Board Delete Run");
+        List<Board> boards=boardRepository.findAllByStatusAndLastModifiedDateBefore(Status.DELETE,LocalDateTime.now().minusYears(5));
+        boardRepository.deleteAll(boards);
     }
 }
