f61825b36391ba6a00fa690580677aef69c7a43e
Hojinida <opqrs1103@gmail.com>
2022-08-04 22:11:32 +0900

    test

--- src/main/java/com/jaks1m/project/domain/common/Schedule.java
+++ src/main/java/com/jaks1m/project/domain/common/Schedule.java
@@ -8,7 +8,6 @@ import com.jaks1m.project.domain.api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
