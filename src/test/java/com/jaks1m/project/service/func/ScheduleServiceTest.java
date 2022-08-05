0ff7d2bde6aca2e6dde503338a567288839f65ea
Hojinida <opqrs1103@gmail.com>
2022-08-06 06:45:12 +0900

    test

---
+++ src/test/java/com/jaks1m/project/service/func/ScheduleServiceTest.java
@@ -0,0 +1,14 @@
+package com.jaks1m.project.service.func;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScheduleServiceTest {
+    @Test
+    public void now(){
+        System.out.println(LocalDate.now());
+    }
+}
