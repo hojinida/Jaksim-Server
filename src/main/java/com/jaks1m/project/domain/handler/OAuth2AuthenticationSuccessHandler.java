d4676bae6d5ad4458bd39cf1af6145e980fa1ea0
Hojinida <opqrs1103@gmail.com>
2022-08-19 20:42:07 +0900

    test

--- src/main/java/com/jaks1m/project/domain/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/jaks1m/project/domain/handler/OAuth2AuthenticationSuccessHandler.java
@@ -42,9 +42,11 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
 
             redisRepository.save(refreshToken);
             response.setStatus(200);
-            response.sendRedirect(UriComponentsBuilder.fromUriString("https://jaks1m.netlify.app/")
-                    .queryParam("accessToken",accessToken)
-                    .queryParam("refreshToken",refreshToken.getValue()).toUriString());
+            response.setHeader("accessToken",accessToken);
+            response.setHeader("refreshToken",refreshToken.getValue());
+//            response.sendRedirect(UriComponentsBuilder.fromUriString("https://jaks1m.netlify.app/")
+//                    .queryParam("accessToken",accessToken)
+//                    .queryParam("refreshToken",refreshToken.getValue()).toUriString());
         }
     }
 }
